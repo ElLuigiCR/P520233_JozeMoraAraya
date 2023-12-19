47c3bf89024a22ac8b6100cdc11e26ab9dbe1ba5
Jozef Mora Araya <25jmafa25@gmail.com>
2023-12-18 19:24:30 -0600

    Initial commit

---
+++ Formularios/FrmLogin.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace P520233_JozeMoraAraya.Formularios
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnIngresoDirecto = new System.Windows.Forms.Button();
+            this.BtnVerContrasennia = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.TxtContrasennia = new System.Windows.Forms.TextBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnIngresoDirecto
+            // 
+            this.BtnIngresoDirecto.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.BtnIngresoDirecto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIngresoDirecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresoDirecto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnIngresoDirecto.Location = new System.Drawing.Point(23, 404);
+            this.BtnIngresoDirecto.Name = "BtnIngresoDirecto";
+            this.BtnIngresoDirecto.Size = new System.Drawing.Size(97, 32);
+            this.BtnIngresoDirecto.TabIndex = 20;
+            this.BtnIngresoDirecto.Text = "Ingresar";
+            this.BtnIngresoDirecto.UseVisualStyleBackColor = false;
+            this.BtnIngresoDirecto.Click += new System.EventHandler(this.BtnIngresoDirecto_Click);
+            // 
+            // BtnVerContrasennia
+            // 
+            this.BtnVerContrasennia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVerContrasennia.Location = new System.Drawing.Point(310, 334);
+            this.BtnVerContrasennia.Name = "BtnVerContrasennia";
+            this.BtnVerContrasennia.Size = new System.Drawing.Size(42, 27);
+            this.BtnVerContrasennia.TabIndex = 19;
+            this.BtnVerContrasennia.Text = "Ver";
+            this.BtnVerContrasennia.UseVisualStyleBackColor = false;
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.BackColor = System.Drawing.Color.Crimson;
+            this.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCerrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnCerrar.Location = new System.Drawing.Point(274, 403);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(92, 32);
+            this.BtnCerrar.TabIndex = 18;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.BackColor = System.Drawing.Color.Orange;
+            this.BtnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnIngresar.Location = new System.Drawing.Point(148, 461);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(103, 21);
+            this.BtnIngresar.TabIndex = 17;
+            this.BtnIngresar.Text = "Entrar Rapido";
+            this.BtnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // TxtContrasennia
+            // 
+            this.TxtContrasennia.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtContrasennia.Location = new System.Drawing.Point(91, 342);
+            this.TxtContrasennia.Name = "TxtContrasennia";
+            this.TxtContrasennia.Size = new System.Drawing.Size(213, 17);
+            this.TxtContrasennia.TabIndex = 14;
+            this.TxtContrasennia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtContrasennia.UseSystemPasswordChar = true;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(91, 261);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(213, 20);
+            this.TxtUsuario.TabIndex = 12;
+            this.TxtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Contrasennia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Usuario";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(109, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 513);
+            this.Controls.Add(this.BtnIngresoDirecto);
+            this.Controls.Add(this.BtnVerContrasennia);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.TxtContrasennia);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FrmLogin";
+            this.Text = "FrmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnIngresoDirecto;
+        private System.Windows.Forms.Button BtnVerContrasennia;
+        private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.Button BtnIngresar;
+        private System.Windows.Forms.TextBox TxtContrasennia;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
